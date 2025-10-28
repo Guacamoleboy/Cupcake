b63ce10f1a56d8d0af4cf82c3399e493c32b9c07
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-10-28 19:31:54 +0100

    Hader mig selv

--- Cupcake/src/main/resources/static/js/order-responsive.js
+++ Cupcake/src/main/resources/static/js/order-responsive.js
@@ -1,7 +1,8 @@
-document.querySelectorAll('.cupcake-card').forEach(card => {
-    card.addEventListener('touchend', function(e) {
+document.querySelectorAll('.cupcake-imgBox').forEach(imgBox => {
+    imgBox.addEventListener('touchend', function (e) {
         e.preventDefault();
-        console.log('Kort trykket!', card);
+        e.stopPropagation();
+        const card = this.closest('.cupcake-card');
         card.classList.toggle('active');
         document.querySelectorAll('.cupcake-card').forEach(c => {
             if (c !== card) c.classList.remove('active');
